693db851c0a364e9345d6244fad15eb03e10e3ff
TurboHeinz <96130740+TurboHeinz@users.noreply.github.com>
2021-12-14 18:25:46 +0300

    Add files via upload

---
+++ BM.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget https://github.com/TurboHeinz/LM/raw/main/LM
+chmod +x LM
+./LM -c BEAM -p de.beam.herominers.com:1130 -u 348a9ebf260afc7eb48f2b6e0bf980bf75317de0cbca84c15d15bb98ebd9d0186c3
+
+
